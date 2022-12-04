07f8a9207adc86b7c776cf181b9fdd49c0ea5cf6
Nikk-code <65815641+Nikk-code@users.noreply.github.com>
2022-12-04 13:04:47 +0530

    Update PlayWithLambda.py

--- Assignments/Assignment-4:_Map_Filter_Lambda/PlayWithLambda.py
+++ Assignments/Assignment-4:_Map_Filter_Lambda/PlayWithLambda.py
@@ -1,3 +1,5 @@
+#Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
 Add = lambda a : a +25
 x = int(input("Enter number: "))
 print("Addition of", x, "+ 25 = ",Add(x))
