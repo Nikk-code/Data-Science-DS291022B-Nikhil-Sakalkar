ba86705430124ba6d1505906d8d04951c1eafd57
Nikk-code <65815641+Nikk-code@users.noreply.github.com>
2022-12-31 22:56:20 +0530

    Create 9_Nested_Lists.py

---
+++ Assignments/Assignment_1_HackerrankQuestions/9_Nested_Lists.py
@@ -0,0 +1,18 @@
+"""
+Nested Lists
+https://www.hackerrank.com/challenges/nested-list/problem
+
+"""
+
+if __name__ == '__main__':
+    students = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        
+        students.append([name, score])
+    mn = min(students, key=lambda x: x[1])
+    nonlowest = sorted([student for student in students if student[1] > mn[1]], key=lambda x: x[1])
+    seconds = sorted([student[0] for student in nonlowest if student[1] == nonlowest[0][1]])
+    for student in seconds:
+        print(student)
