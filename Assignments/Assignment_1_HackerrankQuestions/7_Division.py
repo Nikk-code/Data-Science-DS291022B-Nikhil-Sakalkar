d7783ce115f4c177f90f27401792bd65597ccc6c
Nikk-code <65815641+Nikk-code@users.noreply.github.com>
2022-12-31 22:35:52 +0530

    Create 7_Division.py

---
+++ Assignments/Assignment_1_HackerrankQuestions/7_Division.py
@@ -0,0 +1,19 @@
+"""
+
+Python: Division
+https://www.hackerrank.com/challenges/python-division/problem
+
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    def division_int(x,y):
+        return x // y
+
+    def division_flt(x,y):
+        return x / y
+
+    print(division_int(a,b))
+    print(division_flt(a,b))
