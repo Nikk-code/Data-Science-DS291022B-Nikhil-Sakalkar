e3d26825ebafb5ae554c22988e19ae81ddbdf355
Nikk-code <65815641+Nikk-code@users.noreply.github.com>
2023-01-02 21:28:56 +0530

    Create 20_itertools.combinations().py

---
+++ Assignments/Python Certification Preparation/Assignment_1_HackerrankQuestions/20_itertools.combinations().py
@@ -0,0 +1,14 @@
+'''
+
+itertools.combinations()
+https://www.hackerrank.com/challenges/itertools-combinations/problem
+
+'''
+
+from itertools import combinations
+
+a = input().split()
+
+for i in range(1, int(a[1]) + 1):
+    for j in combinations(sorted(a[0]), i):
+        print (''.join(j))
