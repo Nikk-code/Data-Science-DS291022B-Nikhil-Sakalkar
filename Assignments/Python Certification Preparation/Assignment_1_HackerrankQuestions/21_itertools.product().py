e2fd92ff8db8d0d1c2222a41ed72034c5a858e9f
Nikk-code <65815641+Nikk-code@users.noreply.github.com>
2023-01-02 21:34:02 +0530

    Create 21_itertools.product().py

---
+++ Assignments/Python Certification Preparation/Assignment_1_HackerrankQuestions/21_itertools.product().py
@@ -0,0 +1,12 @@
+'''
+
+itertools.product()
+https://www.hackerrank.com/challenges/itertools-product/problem
+
+'''
+
+from itertools import product
+a = map(int, input().split())
+b = map(int, input().split())
+
+print(*product(a, b))
